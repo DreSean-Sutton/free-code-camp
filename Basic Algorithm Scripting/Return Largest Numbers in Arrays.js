697b8bf1ae91999fc9874b260c019bbf3d8c2807
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-28 05:06:51 -0800

    finished Return Largest Numbers in Arrays algorithm

---
+++ Basic Algorithm Scripting/Return Largest Numbers in Arrays.js
@@ -0,0 +1,14 @@
+function largestOfFour(arr) {
+  debugger;
+  var allHighestNums = [];
+  for (let i = 0; i < arr.length; i++) {
+    var highestNum = arr[i][0];
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > highestNum) {
+        highestNum = arr[i][j];
+      }
+    }
+    allHighestNums.push(highestNum)
+  }
+  return allHighestNums;
+}
