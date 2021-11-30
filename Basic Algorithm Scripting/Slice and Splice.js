a45d8fa13d88329be672eb2d1c30d86c8dc5db74
DreSean-Sutton <dreseansutton@gmail.com>
2021-11-29 23:24:22 -0800

    Finished Slice and Splice algorithm

---
+++ Basic Algorithm Scripting/Slice and Splice.js
@@ -0,0 +1,17 @@
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n, 0, arr1[i])
+    n++
+  }
+  return newArr;
+}
+
+// Quicker version of function below
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
