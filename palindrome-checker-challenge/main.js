2897f985c9274c5bd5a52281410a457c7416a1e2
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-12 13:29:17 -0800

    Finished challenge

--- palindrome-checker-challenge/main.js
+++ palindrome-checker-challenge/main.js
@@ -1,3 +1,12 @@
 function palindrome(str) {
-  return true;
+  let result = '';
+  let reverseResult = '';
+  let myRegex = /[a-z0-9]/gi;
+  result = str.toLowerCase().match(myRegex);
+  reverseResult = result.slice(0).reverse();
+  if (result.join('') === reverseResult.join('')) {
+    return true;
+  } else {
+    return false;
+  }
 }
