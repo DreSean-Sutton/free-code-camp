f8994b23c2199b5feb56cdafd06cd04b6eca3d47
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-13 11:26:52 -0800

    Finished Roman Numeral Converter Challenge

---
+++ roman-numeral-converter-challenge/main.js
@@ -0,0 +1,52 @@
+/*
+-assign the value of an empty string to variable romanNumeral
+-create a for loop that:
+  a. assigns the value of num to i
+  b. executes code block if i is greater than 0
+  -check if i is greater than 1000 and if it is:
+    += the value of 'M' to romanNumeral
+    subtract i by 1000
+  -else if i is equal to or greater than 900
+    += the value of 'CM' to romanNumeral
+    subtract i by 900
+  -else if i is equal to or greater than 500
+    += the value of 'D' to romanNumeral
+    subtract i by 500
+  -else if i is equal to or greater than 400
+    += the value of 'CD' to romanNumeral
+  subtract i by 400
+  -else if i is equal to or greater than 100
+    += the value of 'C' to romanNumeral
+    subtract i by 100
+  -else if i is equal to or greater than 90
+    += the value of 'XC' to romanNumeral
+    subtract i by 90
+  -else if i is equal to or greater than 50
+    += the value of 'L' to romanNumeral
+    subtract i by 50
+  -else if i is equal to or greater than 40
+    += the value of 'XL' to romanNumeral
+  -else if i is equal to or greater than 10
+    += the value of 'X' to romanNumeral
+    subtract i by 10
+  -else if i is equal to or greater than 9
+    += the value of 'IX' to romanNumeral
+    subtract i by 9
+  -else if i is equal to or greater than 5
+    += the value of 'V' to romanNumeral
+    subtract i by 5
+  -else if i is equal to or greater than 4
+    += the value of 'IV' to romanNumeral
+    subtract i by 4
+  -else if i is equal to or greater than 1
+    += the value of 'I' to romanNumeral
+    subtract i by 1
+-return romanNumeral from the function
+    */
+
+function convertToRoman(num) {
+
+  return romanNumeral
+}
+
+convertToRoman(36);
