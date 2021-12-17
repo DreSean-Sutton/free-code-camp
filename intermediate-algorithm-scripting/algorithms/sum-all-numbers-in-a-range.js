c40bc694915011a5d6a121335f56b2d1fa2215d5
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-17 12:04:07 -0800

    Finished sum-of-all algorithm

--- intermediate-algorithm-scripting/algorithms/sum-all-numbers-in-a-range.js
+++ intermediate-algorithm-scripting/algorithms/sum-all-numbers-in-a-range.js
@@ -1,5 +1,45 @@
+/*
+-create a variable named twoArr and assign it the value of an empty array
+-create a variable named sumOfAll and assign it the value of 0
+-if arr at position 0 is greater than arr at position 1:
+  assign the value of [arr[1], arr[0]] to twoArr
+-else
+  assign the value of arr to twoArr
+create a for loop that:
+  a. assigns the value of twoArr[0] to variable i
+  b. executes code block if i is less than or equal to twoArr[1]
+  c. increments i by 1
+  sumOfAll += i
+-return sumOfAll
+*/
+
 function sumAll(arr) {
-  return 1;
+  let twoArr = [];
+  let sumOfAll = 0;
+
+  if (arr[0] > arr[1]) {
+    twoArr = [arr[1], arr[0]]
+  } else {
+    twoArr = [arr[0], arr[1]];
+  }
+  for (let i = twoArr[0]; i <= twoArr[1]; i++) {
+    sumOfAll += i
+  }
+  return sumOfAll;
+}
+
+console.log(sumAll([1, 4]));
+
+// ANOTHER WAY TO DO THE SAME FUNCTION BELOW
+
+function sumAll(arr) {
+  let max = Math.max(arr[0], arr[1]);
+  let min = Math.min(arr[0], arr[1]);
+  let sumOfAll = 0;
+  for (let i = min; i <= max; i++) {
+    sumOfAll += i;
+  }
+  return sumOfAll;
 }
 
 sumAll([1, 4]);
