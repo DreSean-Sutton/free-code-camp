52da21d23665d5009be515f757bcdd24954ad8ce
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-23 20:41:31 -0800

    Finished convert-html-entities algorithm and set up sum-all-odd-fibonacci-numbers algorithm

--- intermediate-algorithm-scripting/algorithms/convert-html-entities.js
+++ intermediate-algorithm-scripting/algorithms/convert-html-entities.js
@@ -1,5 +1,42 @@
+/*
+create a variable named newStr and assign it the value of an empty string
+-create a for loop that:
+  a. assigns the value of 0 to variable i
+  b. executes code block if i is less than str.length
+  c. i++
+  -if str[i] is strictly equal to '
+    newStr += &apos;
+  -else if str[i] is strictly equal to "
+    newStr += &quot;
+  -else if str[i] is strictly equal to >
+    newStr += &gt;
+  -else if str[i] is strictly equal to <
+    newStr += &lt;
+  -else if str[i] is strictly equal to &
+    newStr += &amp;
+  -else:
+    newStr += str[i]
+return newStr
+*/
+
 function convertHTML(str) {
-  return str;
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '\'') {
+      newStr += '&apos;';
+    } else if (str[i] === '\"') {
+      newStr += '&quot;';
+    } else if (str[i] === '\>') {
+      newStr += '&gt;';
+    } else if (str[i] === '\<') {
+      newStr += '&lt;';
+    } else if (str[i] === '\&') {
+      newStr += '&amp;';
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
 }
 
 convertHTML("Dolce & Gabbana");
