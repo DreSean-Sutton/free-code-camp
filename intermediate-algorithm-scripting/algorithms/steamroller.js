99c1d3a80c94c9cfe89ce836468fe850293e0737
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-26 19:21:46 -0800

    Finished steamroller algorithm and set up binary-agents algorithm

--- intermediate-algorithm-scripting/algorithms/steamroller.js
+++ intermediate-algorithm-scripting/algorithms/steamroller.js
@@ -1,5 +1,13 @@
 function steamrollArray(arr) {
-  return arr;
-}
+  const flattenedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+};
 
-steamrollArray([1, [2], [3, [[4]]]]);
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
