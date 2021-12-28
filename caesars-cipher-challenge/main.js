5e3f04d0022092f7f6f73ed7173300bcc42341d5
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-28 02:13:15 -0800

    Finished caesars-cipher-challenge algorithm

--- caesars-cipher-challenge/main.js
+++ caesars-cipher-challenge/main.js
@@ -1,9 +1,36 @@
 /*
--Go through each letter, get its code and add or subtract 13 from it
+-create a variable named currentCharCode and assign it the value of null
+create a variable named newStr and assign it the value of an empty string
+create a for loop that:
+  a. assigns the value of 0 to i
+  b. execute code block if i is less than str.length
+  c. i ++
+  -if str[i].match(/\W/)
+    newStr += str[i]
+  -else if str.charCodeAt(i) + 13 is greater than 90
+    currentCharCode = str.charCodeAt(i) - 13
+    newStr += String.fromCharCode(currentCharCode)
+  -else:
+    currentCharCode = str.charCodeAt(i) + 13
+    newStr += String.fromCharCode(currentCharCode)
+-return newStr
 */
 
 function rot13(str) {
-  return str;
+  let currentCharCode = null;
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(/\W/)) {
+      newStr += str[i]
+    } else if (str.charCodeAt(i) + 13 > 90) {
+      currentCharCode = str.charCodeAt(i) - 13
+      newStr += String.fromCharCode(currentCharCode)
+    } else {
+      currentCharCode = str.charCodeAt(i) + 13
+      newStr += String.fromCharCode(currentCharCode)
+    }
+  }
+  return newStr;
 }
 
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));
